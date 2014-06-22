b95d54d73ebd967209d0f455084a41984bf5373c
DoubleDFresh <duncan.doug@gmail.com>
2014-06-22 12:40:40 -0500

    upload initial test data script

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,12 @@
 ## Merge the training and test sets to create one data set.
 
+y_test <- read.table("./data/UCI HAR Dataset/test/y_test.txt", header = FALSE)
+X_test <- read.table("./data/UCI HAR Dataset/test/X_test.txt", header = FALSE)
+features <- read.table("./data/UCI HAR Dataset/features.txt", header = FALSE, col.names = c("indx", "feature_col"), stringsAsFactors = FALSE)
+features <- transform(features, feature_col = make.names(feature_col))
+subject_test <- read.table("./data/UCI HAR Dataset/test/subject_test.txt", header = FALSE)
+activity_labels <- read.table("./data/UCI HAR Dataset/activity_labels.txt", header = FALSE, colClasses = "character")
+
 
 ## Extract only the measurements on the mean and standard devication for each measurement
 
