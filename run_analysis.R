8b8741aaa71b0c87b459ed1a250b2f0a6e2f0c15
DoubleDFresh <duncan.doug@gmail.com>
2014-06-22 09:02:26 -0500

    uploading initial R script

---
+++ run_analysis.R
@@ -0,0 +1,15 @@
+## Merge the training and test sets to create one data set.
+
+
+## Extract only the measurements on the mean and standard devication for each measurement
+
+
+## Uses descriptive activity names to name the activities in the data set
+
+
+## Appropriately labels the data set with descriptive variable names. 
+
+
+## Creates a second, independent tidy data set with the average of each variable for each activity 
+#  and each subject.
+
